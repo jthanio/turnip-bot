afd9d2d8b25096fcd62215bb692e5c17328c83a7
jthanio <64034486+jthanio@users.noreply.github.com>
2020-04-27 18:07:10 -0300

    first working implementation

---
+++ access.go
@@ -0,0 +1,286 @@
+package main
+
+import (
+	"database/sql"
+	"errors"
+	"fmt"
+	"time"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+const (
+	sqlUserTableName   = "user"
+	sqlUserID          = "user_id"
+	sqlUserDiscordID   = "discord_user_id"
+	sqlUserDiscordName = "discord_user_name"
+	sqlWeekTableName   = "week"
+	sqlWeekID          = "week_id"
+	sqlWeekStartDay    = "week_start_day"
+	sqlWeekStartPrice  = "week_start_price"
+	sqlPriceTableName  = "price"
+	sqlPriceID         = "price_id"
+	sqlPriceDay        = "price_day"
+	sqlPriceTime       = "price_time"
+	sqlPriceSell       = "price_sell"
+	timeFormat         = "2006-01-02"
+)
+
+// TurnipAccess stores connection information for the turnip db.
+type TurnipAccess struct {
+	db *sql.DB
+}
+
+// NewTurnipAccess creates a database access object for the turnip db.
+func NewTurnipAccess() (*TurnipAccess, error) {
+	access := &TurnipAccess{}
+	var err error
+	access.db, err = sql.Open("sqlite3", "./turnips.db")
+	if err != nil {
+		return nil, err
+	}
+
+	return access, nil
+}
+
+// CreateTables creates all of the tables in the turnip tb.
+func (a *TurnipAccess) CreateTables() error {
+	tx, err := a.db.Begin()
+	if err != nil {
+		return err
+	}
+
+	createUserTable, err := tx.Prepare(fmt.Sprintf(`CREATE TABLE IF NOT EXISTS %s (%s INTEGER NOT NULL PRIMARY KEY, %s TEXT NOT NULL, %s TEXT NOT NULL);`, sqlUserTableName, sqlUserID, sqlUserDiscordID, sqlUserDiscordName))
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	createWeekTable, err := tx.Prepare(fmt.Sprintf(`CREATE TABLE IF NOT EXISTS %s (%s INTEGER NOT NULL PRIMARY KEY, %s INTEGER NOT NULL, %s TEXT NOT NULL, %s INTEGER NOT NULL, FOREIGN KEY(%s) REFERENCES %s(%s));`, sqlWeekTableName, sqlWeekID, sqlUserID, sqlWeekStartDay, sqlWeekStartPrice, sqlUserID, sqlUserTableName, sqlUserID))
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	createPriceTable, err := tx.Prepare(fmt.Sprintf(`CREATE TABLE IF NOT EXISTS %s (%s INTEGER NOT NULL PRIMARY KEY, %s INTEGER NOT NULL, %s INTEGER NOT NULL, %s TEXT NOT NULL, %s INTEGER NOT NULL, FOREIGN KEY(%s) REFERENCES %s(%s));`, sqlPriceTableName, sqlPriceID, sqlWeekID, sqlPriceDay, sqlPriceTime, sqlPriceSell, sqlWeekID, sqlWeekTableName, sqlWeekID))
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	if _, err := createUserTable.Exec(); err != nil {
+		tx.Rollback()
+		return err
+	}
+	if _, err := createWeekTable.Exec(); err != nil {
+		tx.Rollback()
+		return err
+	}
+	if _, err := createPriceTable.Exec(); err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	return tx.Commit()
+}
+
+type weekObject struct {
+	id         int
+	userID     int
+	startDay   string
+	startPrice int
+}
+
+func getNearestSunday(day time.Time) (time.Time, error) {
+	wDay := day.Weekday()
+	if wDay != time.Sunday {
+		day.AddDate(0, 0, -int(wDay)) // Subtract days past Sunday to get most recent Sunday.
+	}
+
+	return day, nil
+}
+
+// GetWeek checks the db if week exists. If one is found, then the weekID is returned
+func (a *TurnipAccess) GetWeek(userID int, day time.Time) (int, error) {
+	var err error
+	day, err = getNearestSunday(day)
+	if err != nil {
+		return 0, err
+	}
+	sunday := day.Format(timeFormat)
+
+	// Prepare the query to check if the price exists already
+	checkWeek, err := a.db.Prepare(fmt.Sprintf(`SELECT %s FROM %s WHERE %s = ? AND %s = ?;`, sqlWeekID, sqlWeekTableName, sqlUserID, sqlWeekStartDay))
+	if err != nil {
+		return 0, err
+	}
+
+	// Check if the price is already provided and update if so
+	row := checkWeek.QueryRow(userID, sunday)
+	var id64 int64
+	if err := row.Scan(&id64); err != nil {
+		return 0, err
+	}
+
+	return int(id64), nil
+}
+
+// CreateOrUpdateWeek checks the db if the week exists or creates a new week if not. In both cases with no error, the week ID is returned.
+func (a *TurnipAccess) CreateOrUpdateWeek(userID int, day time.Time, price int) (int, error) {
+	sundayTime, err := getNearestSunday(day)
+	if err != nil {
+		return 0, err
+	}
+	sunday := sundayTime.Format(timeFormat)
+
+	weekID, err := a.GetWeek(userID, day)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			// Prepare the query to insert the price
+			insertSellPrice, err := a.db.Prepare(fmt.Sprintf(`INSERT INTO %s (%s, %s, %s) VALUES (?, ?, ?);`, sqlWeekTableName, sqlUserID, sqlWeekStartDay, sqlWeekStartPrice))
+			if err != nil {
+				return 0, err
+			}
+
+			// Run the prepared query
+			res, err := insertSellPrice.Exec(userID, sunday, price)
+			if err != nil {
+				return 0, err
+			}
+
+			// Get the ID of the price
+			id64, err := res.LastInsertId()
+			if err != nil {
+				return 0, err
+			}
+
+			return int(id64), nil
+		}
+		return 0, err
+	}
+
+	// Update the sell price on the existing week
+	updateSellPrice, err := a.db.Prepare(fmt.Sprintf(`UPDATE %s SET %s = ? WHERE %s = ?;`, sqlWeekTableName, sqlWeekStartPrice, sqlWeekID))
+	if err != nil {
+		return 0, err
+	}
+
+	// Run the prepared query
+	if _, err := updateSellPrice.Exec(price, weekID); err != nil {
+		return 0, err
+	}
+
+	return weekID, nil
+}
+
+type priceObject struct {
+	id     int
+	weekID int
+	day    int
+	time   string
+	sell   int
+}
+
+// CreateOrUpdateBuyPrice checks the db if the price exists or creates a new price if not.
+// If the price exists and the values are different, the price is updated.
+func (a *TurnipAccess) CreateOrUpdateBuyPrice(weekID int, dayNum int, meridian string, price int) (int, error) {
+	// Validate the meridian string
+	if meridian != "am" && meridian != "pm" {
+		return 0, fmt.Errorf("string \"%s\" must be am or pm", meridian)
+	}
+
+	// Prepare the query to check if the price exists already
+	checkBuyPrice, err := a.db.Prepare(fmt.Sprintf(`SELECT %s FROM %s WHERE %s = ? AND %s = ? AND %s = ?;`, sqlPriceID, sqlPriceTableName, sqlWeekID, sqlPriceDay, sqlPriceTime))
+	if err != nil {
+		return 0, err
+	}
+
+	// Check if the price is already provided and update if so
+	row := checkBuyPrice.QueryRow(weekID, dayNum, meridian)
+	var w priceObject
+	if err := row.Scan(&w); err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			// Prepare the query to insert the price
+			insertBuyPrice, err := a.db.Prepare(fmt.Sprintf(`INSERT INTO %s (%s, %s, %s, %s) VALUES (?, ?, ?, ?);`, sqlPriceTableName, sqlWeekID, sqlPriceDay, sqlPriceTime, sqlPriceSell))
+			if err != nil {
+				return 0, err
+			}
+
+			// Run the prepared query
+			res, err := insertBuyPrice.Exec(weekID, dayNum, meridian, price)
+			if err != nil {
+				return 0, err
+			}
+
+			// Get the ID of the price
+			id64, err := res.LastInsertId()
+			if err != nil {
+				return 0, err
+			}
+
+			return int(id64), nil
+		}
+		return 0, err
+
+	}
+
+	// Prepare the query to update the price
+	updateBuyPrice, err := a.db.Prepare(fmt.Sprintf(`UPDATE %s SET %s = ? WHERE %s = ?;`, sqlPriceTableName, sqlPriceSell, sqlPriceID))
+	if err != nil {
+		return 0, err
+	}
+
+	// Run the prepared query
+	if _, err := updateBuyPrice.Exec(price, w.id); err != nil {
+		return 0, err
+	}
+
+	return w.id, nil
+}
+
+// GetOrCreateUser checks the db if the user exists or creates a new user if not. In both cases with no error, the user ID is returned.
+func (a *TurnipAccess) GetOrCreateUser(discordUserID string, discordUserName string) (int, error) {
+
+	// Prepare the query to check if the user exists already
+	checkUser, err := a.db.Prepare(fmt.Sprintf(`SELECT %s FROM %s WHERE %s = ?;`, sqlUserID, sqlUserTableName, sqlUserDiscordID))
+	if err != nil {
+		return 0, err
+	}
+
+	// Check if the user is already registered and exit if so
+	row := checkUser.QueryRow(discordUserID)
+	var id int
+	if err := row.Scan(&id); err != nil && !errors.Is(err, sql.ErrNoRows) {
+		return 0, err
+	}
+
+	// User is already registered
+	if id > 0 {
+		return id, nil
+	}
+
+	// Prepare the query to save the new user
+	insertUser, err := a.db.Prepare(fmt.Sprintf(`INSERT INTO %s (%s, %s) VALUES (?, ?);`, sqlUserTableName, sqlUserDiscordID, sqlUserDiscordName))
+	if err != nil {
+		return 0, err
+	}
+
+	// Run the prepared query
+	res, err := insertUser.Exec(discordUserID, discordUserName)
+	if err != nil {
+		return 0, err
+	}
+
+	// Get the ID of the user
+	id64, err := res.LastInsertId()
+	if err != nil {
+		return 0, err
+	}
+
+	return int(id64), nil
+}
+
+// Close closes the stored db connection.
+func (a *TurnipAccess) Close() error {
+	return a.db.Close()
+}
--- discord.go
+++ discord.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"database/sql"
 	"encoding/json"
 	"fmt"
 	"os"
@@ -10,6 +9,7 @@ import (
 	"time"
 
 	"github.com/bwmarrin/discordgo"
+	"github.com/pkg/errors"
 )
 
 type discordConf struct {
@@ -19,6 +19,36 @@ type discordConf struct {
 
 type config struct {
 	Discord discordConf `json:"discord"`
+}
+
+// DiscordBot controls the turnip bot and stores all required connection details.
+type DiscordBot struct {
+	access *TurnipAccess
+	dg     *discordgo.Session
+}
+
+// NewDiscordBot creates an active session for a discord bot.
+func NewDiscordBot(access *TurnipAccess) (*DiscordBot, error) {
+	token := loadToken()
+	dg, err := discordgo.New("Bot " + token)
+	if err != nil {
+		errors.Wrap(err, "error creating Discord session:")
+		return nil, err
+	}
+
+	var bot = &DiscordBot{dg: dg, access: access}
+
+	// Add handlers for the different events
+	dg.AddHandler(bot.messageCreateHook())
+
+	// Open a websocket connection to Discord and begin listening.
+	err = dg.Open()
+	if err != nil {
+		errors.Wrap(err, "error opening connection:")
+		return nil, err
+	}
+
+	return bot, nil
 }
 
 func loadToken() string {
@@ -37,46 +67,144 @@ func loadToken() string {
 	return config.Discord.Token
 }
 
-func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
-	// Ignore all messages created by the bot itself
-	// This isn't required in this specific example but it's a good practice.
-	if m.Author.ID == s.State.User.ID {
-		return
-	}
-
-	buyCmd := regexp.MustCompile(`^!buy`)
-	sellCmd := regexp.MustCompile(`^!sell`)
-	priceRE := regexp.MustCompile(`\d+`)
-
-	switch {
-	case buyCmd.MatchString(m.Content):
-		test := priceRE.FindString(m.Content)
-		fmt.Println(test)
-		price, err := strconv.Atoi(test)
-		if err != nil {
-			fmt.Println("unable to parse integer: ", err)
+func (d *DiscordBot) messageCreateHook() func(s *discordgo.Session, m *discordgo.MessageCreate) {
+	return func(s *discordgo.Session, m *discordgo.MessageCreate) {
+		// Ignore all messages created by the bot itself
+		// This isn't required in this specific example but it's a good practice.
+		if m.Author.ID == s.State.User.ID {
+			return
 		}
-		fmt.Println(fmt.Sprintf("got buy price %d", price))
-		s.MessageReactionAdd(m.ChannelID, m.ID, "🤖")
-		//s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("Got buy price %d", price))
-	case sellCmd.MatchString(m.Content):
-		test := priceRE.FindString(m.Content)
-		fmt.Println(test)
-		price, _ := strconv.Atoi(test)
-		fmt.Println(fmt.Sprintf("got sell price %d", price))
-		s.MessageReactionAdd(m.ChannelID, m.ID, "🤖")
-		//s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("Got sell price: %d", price))
+
+		buyCmd := regexp.MustCompile(`^!buy\s*\d+\s*([mM]onday|[tT]uesday|[wW]ednesday|[tT]hursday|[fF]riday|[sS]aturday)?\s*(am|pm)?`)
+		sellCmd := regexp.MustCompile(`^!sell\s*\d+`)
+
+		switch {
+		case buyCmd.MatchString(m.Content):
+			err := d.buyCommand(s, m)
+			if err != nil {
+				fmt.Println("buy command: ", err)
+				return
+			}
+		case sellCmd.MatchString(m.Content):
+			err := d.sellCommand(s, m)
+			if err != nil {
+				fmt.Println("sell command: ", err)
+				return
+			}
+		}
 	}
 }
 
-func storeBuyPrice(db *sql.DB, userid string, day time.Time, isPMPrice bool) error {
-	tx, err := db.Begin()
+func (d *DiscordBot) buyCommand(s *discordgo.Session, m *discordgo.MessageCreate) error {
+	priceArg := regexp.MustCompile(`\d+`)
+	dayArg := regexp.MustCompile(`\b([mM]onday|[tT]uesday|[wW]ednesday|[tT]hursday|[fF]riday|[sS]aturday)\b`)
+	meridianArg := regexp.MustCompile(`\b(am|pm)\b`)
+
+	// Parse price
+	rawPrice := priceArg.FindString(m.Content)
+	price, err := strconv.Atoi(rawPrice)
 	if err != nil {
-		return err
+		return fmt.Errorf("unable to parse price: %w", err)
+	}
+	fmt.Println(fmt.Sprintf("got buy price %d", price))
+
+	// Parse date
+	messageCreateTime, err := snowflakeCreationTime(m.ID) // Get the day based on message creation time
+	if err != nil {
+		return fmt.Errorf("unable to get message timestamp: %w", err)
 	}
 
-	day.Format("2006-01-02 15:04:05")
-	insertBuyPrice := `insert into turnips (userid, time, buy_price) values ($1, $2, $3)`
-	tx.Exec(insertBuyPrice, userid)
+	var weekdayNum int
+	day := dayArg.FindString(m.Content)
+	if day != "" {
+		weekday, err := time.Parse("Monday", day)
+		if err != nil {
+			return err
+		}
+		weekdayNum = int(weekday.Weekday())
+	} else {
+		weekdayNum = int(messageCreateTime.Weekday()) // If no day provided, infer weekday from message create time
+	}
+
+	// parse am/pm
+	meridian := meridianArg.FindString(m.Content)
+
+	if meridian == "" {
+		// If no am/pm provided, infer from message create time
+		switch {
+		case messageCreateTime.Hour() < 11:
+			meridian = "am"
+		case messageCreateTime.Hour() >= 11:
+			meridian = "pm"
+		}
+	}
+
+	// Check if user already exists
+	userID, err := d.access.GetOrCreateUser(m.Author.ID, m.Author.Username)
+	if err != nil {
+		return fmt.Errorf("unable to get user registry: %w", err)
+	}
+
+	// Check if week already exists
+	weekID, err := d.access.GetWeek(userID, messageCreateTime)
+	if err != nil {
+		s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("%s please provide your sell price for the week before posting buy prices.", m.Author.Mention()))
+		return fmt.Errorf("unable to get week for user: %w", err)
+	}
+
+	// Create or update the buy price
+	if _, err := d.access.CreateOrUpdateBuyPrice(weekID, weekdayNum, meridian, price); err != nil {
+		return fmt.Errorf("unable to save buy for week: %w", err)
+	}
+
+	s.MessageReactionAdd(m.ChannelID, m.ID, "🤖")
 	return nil
+}
+
+func (d *DiscordBot) sellCommand(s *discordgo.Session, m *discordgo.MessageCreate) error {
+	priceArg := regexp.MustCompile(`\d+`)
+
+	// Parse price
+	rawPrice := priceArg.FindString(m.Content)
+	price, err := strconv.Atoi(rawPrice)
+	if err != nil {
+		return fmt.Errorf("unable to parse price: %w", err)
+	}
+
+	// Parse date
+	messageCreateTime, err := snowflakeCreationTime(m.ID) // Get the day based on message creation time
+	if err != nil {
+		return fmt.Errorf("unable to get message timestamp: %w", err)
+	}
+
+	// Check if user already exists
+	userID, err := d.access.GetOrCreateUser(m.Author.ID, m.Author.Username)
+	if err != nil {
+		return fmt.Errorf("unable to get user registry: %w", err)
+	}
+
+	// Create or update the week (sell price)
+	if _, err := d.access.CreateOrUpdateWeek(userID, messageCreateTime, price); err != nil {
+		return fmt.Errorf("unable to create week for user: %w", err)
+	}
+
+	fmt.Println(fmt.Sprintf("got sell price %d", price))
+	s.MessageReactionAdd(m.ChannelID, m.ID, "🤖")
+	return nil
+}
+
+// Close closes the discord connections on the bot.
+func (d *DiscordBot) Close() error {
+	return d.dg.Close()
+}
+
+// snowflakeCreationTime extracts the timestamp from the snowflake (discord ID system)
+func snowflakeCreationTime(ID string) (t time.Time, err error) {
+	i, err := strconv.ParseInt(ID, 10, 64)
+	if err != nil {
+		return
+	}
+	timestamp := (i >> 22) + 1420070400000
+	t = time.Unix(timestamp/1000, 0)
+	return
 }
